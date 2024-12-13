16144b9acde7ddf60628ea498f5d73192f59016c
Aenon || Noni <114278519+Srmntnn@users.noreply.github.com>
2024-12-13 19:06:24 +0800

    Update vite.config.js

--- HelpFurr/vite.config.js
+++ HelpFurr/vite.config.js
@@ -1,21 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-// // server: {
-// //   port: 5173,
-// //   proxy: {
-// //     '/auth': {
-// //       target: 'http://localhost:8080',
-// //       changeOrigin: true
-// //     }
-// //   }
-// // }
-
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
@@ -26,18 +8,6 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  server: {
-    // hmr: {
-    //   overlay: false, // Disable the HMR overlay
-    // },
-    port: 5173,
-     proxy: {
-       '/auth': {
-         target: 'http://localhost:8080',
-         changeOrigin: true
-       }
-     }
   },
 }) 
 
